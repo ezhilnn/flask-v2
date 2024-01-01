64e4ff7f14c4f7691c6bb8ea5964f9272e188e25
ezhilnn <113933914+ezhilnn@users.noreply.github.com>
2024-01-01 20:21:09 +0000

    added api rouotr

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 app = Flask(__name__)
 JOBS = [
     {'id': 1, 'title': 'Python Developer', 'location': 'New York', 'salary': '$100000'},
@@ -9,6 +9,8 @@ JOBS = [
 @app.route('/')
 def hello_world():
   return render_template('home.html',jobs=JOBS)
-
+@app.route('/api/jobs')
+def jobs_json():
+  return jsonify(JOBS)
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
